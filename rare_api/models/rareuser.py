1043606e53c16fcdaa3f06713c59932a1f4b9a24
Val Currie <contact@va.lcuri.email>
2021-08-16 14:14:22 -0500

    added docstring -- simple since it's a simple class

--- rare_api/models/rareuser.py
+++ rare_api/models/rareuser.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 
 
 class RareUser(models.Model):
+    """Represents a user of the Rare application"""
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=255)
     profile_image_url = models.CharField(max_length=255)
