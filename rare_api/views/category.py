0bf467323709c9bf9b112e15acb7a59595c259f4
alambrow <alexander.lambrow@gmail.com>
2021-08-16 12:38:11 -0500

    delete category implemented

--- rare_api/views/category.py
+++ rare_api/views/category.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ViewSet
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rare_api.models import Category
 from rest_framework.response import Response
 from django.http import HttpResponseServerError
@@ -25,6 +25,17 @@ class CategoryView(ViewSet):
             return Response(serializer.data)
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        """Handles DELETE requests for single category"""
+        try:
+            category = Category.objects.get(pk=pk)
+            category.delete()
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 class CategorySerializer(serializers.ModelSerializer):
     
