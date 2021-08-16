10e51c3972fc2c8c55c0f39f7a2c07a421f3f689
alambrow <alexander.lambrow@gmail.com>
2021-08-16 14:22:46 -0500

    reactions get all, delete implemented

--- rare_api/views/reaction.py
+++ rare_api/views/reaction.py
@@ -15,6 +15,29 @@ class ReactioView(ViewSet):
             reactions, many=True, context={'request': request}
         )
         return Response(serializer.data)
+    
+    def create(self, request):
+        reaction = Reaction()
+        reaction.label = request.data["label"]
+        reaction.image_url = request.data["image_url"]
+        try:
+            reaction.save()
+            serializer = ReactionSerializer(reaction, context={'request': request})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def destroy(self, request, pk=None):
+        try:
+            reaction = Reaction.objects.get(pk=pk)
+            reaction.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Reaction.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 class ReactionSerializer(serializers.ModelSerializer):
     class Meta:
