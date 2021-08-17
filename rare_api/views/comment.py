f2a5288b5684b118efe084b3e0c8e96cb424b70a
S-L-Murphey <smurphey1990@gmail.com>
2021-08-17 10:15:33 -0500

    added necessary viewsets to tags and comments

--- rare_api/views/comment.py
+++ rare_api/views/comment.py
@@ -6,20 +6,28 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from rare_api.models import Comment, User, Post
+from rare_api.models import Comment, RareUser, Post
 
 class CommentView(ViewSet):
     """Comment View"""
 
-    def create(self, request):
-       """Handle POST operations
+    def list(self, request):
+        """Handle GET requests to get all comments
 
         Returns:
-            Response -- JSON serialized game instance
+            Response -- JSON serialized list of comments
         """
+        comments = Comment.objects.all()
 
-        # Uses the token passed in the `Authorization` header
-        user = User.objects.get(user=request.auth.user)
+        serializer = CommentSerializer(
+            comments, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def create(self, request):
+       
+
+        #Uses the token passed in the `Authorization` header
+        user = RareUser.objects.get(user=request.auth.user)
         post = Post.objects.get(pk=request.data["post"])   
         comment = Comment()
         comment.content = request.data["content"]
@@ -37,6 +45,43 @@ class CommentView(ViewSet):
         # client that something was wrong with its request data
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        """Handle DELETE requests for a single game
+
+        Returns:
+            Response -- 200, 404, or 500 status code
+        """
+        try:
+            comment = Comment.objects.get(pk=pk)
+            comment.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Comment.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def update(self, request, pk=None):
+        """Handle PUT requests for an event
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+        comment = Comment.objects.get(pk=pk)
+
+        user = RareUser.objects.get(user=request.auth.user)
+        post = Post.objects.get(pk=request.data["post"])  
+        comment.content = request.data["content"]
+        comment.author = user
+        comment.post = post
+
+        comment.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
 
 class CommentSerializer(serializers.ModelSerializer):
 
--- rare_api/views/tag.py
+++ rare_api/views/tag.py
@@ -6,40 +6,41 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from rare_api.models import Tag, User
+from rare_api.models import Tag, RareUser
 
 class TagView(ViewSet):
     """Tag View"""
 
-    def create(self, request):
-       """Handle POST operations
+    def list(self, request):
+        """Handle GET requests to get all game types
 
         Returns:
-            Response -- JSON serialized game instance
+            Response -- JSON serialized list of game types
         """
+        tags = Tag.objects.all()
 
-        # Uses the token passed in the `Authorization` header
-        user = User.objects.get(user=request.auth.user)
-          
+        serializer = TagSerializer(
+            tags, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    def create(self, request):
+       
+        user = RareUser.objects.get(user=request.auth.user)
         
         tag = Tag()
         tag.label = request.data["label"]
-        
-        
         
         try:
             tag.save()
             serializer = TagSerializer(tag, context={'request': request})
             return Response(serializer.data)
 
-        # If anything went wrong, catch the exception and
-        # send a response with a 400 status code to tell the
-        # client that something was wrong with its request data
+        
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
-class TagSerializer(serializers.ModelSerializer):
 
+class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
