03b84cd77471f4ff3d3f3572afdb521f132d01cc
S-L-Murphey <smurphey1990@gmail.com>
2021-08-18 10:21:49 -0500

    Updated posttags views

--- rare_api/views/post_tag.py
+++ rare_api/views/post_tag.py
@@ -1,23 +1,60 @@
-"""View module for handling requests about categories"""
 from django.core.exceptions import ValidationError
-from django.views.generic.base import RedirectView
 from rest_framework import status
 from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from rare_api.models import PostTag
+from rest_framework import status
+from rare_api.models import Post, Tag, PostTag
 
 class PostTagView(ViewSet):
-    """GamerRater images"""
-
+    
     def create(self, request):
-        posttag = PostTag()
-        category.label = request.data["label"]
+        post_tag = PostTag()
+        post_tag.post = Post.objects.get(pk=request.data["post"])
+        post_tag.tag = Tag.objects.get(pk=request.data["tag"])
 
         try:
-            category.save()
-            serializer = CategorySerializer(category, context={'request': request})
+            post_tag.save()
+            serializer = PostTagSerializer(post_tag, context={'request': request})
             return Response(serializer.data)
         except ValidationError as ex:
             return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    def update(self, request, pk=None):
+        post_tag = PostTag.objects.get(pk=pk)
+        post_tag.post = Post.objects.get(pk=request.data["post"])
+        post_tag.tag = Tag.objects.get(pk=request.data["tag"])
+        post_tag.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def retrieve(self, request, pk=None):
+        try:
+            post_tag = PostTag.objects.get(pk=pk)
+            serializer = PostTagSerializer(post_tag, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+    
+    def destroy(self, request, pk=None):
+        try:
+            post_tag = PostTag.objects.get(pk=pk)
+            post_tag.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+        except Tag.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+
+class PostTagSerializer(serializers.ModelSerializer):
+    reaction = TagSerializer(many=False)
+    class Meta:
+        model = PostTag
+        fields = '__all__'
