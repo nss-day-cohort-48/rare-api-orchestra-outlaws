e8a9126f3dcb834422af52c63e68aac711904a28
S-L-Murphey <smurphey1990@gmail.com>
2021-08-19 15:54:30 -0500

    Pushing Up Code - Comments ViewSet

--- rare_api/models/comment.py
+++ rare_api/models/comment.py
@@ -5,3 +5,13 @@ class Comment(models.Model):
     post = models.ForeignKey("Post", on_delete=models.CASCADE)
     author = models.ForeignKey("RareUser", on_delete=models.CASCADE)
     content = models.CharField(max_length=1000)
+    publication_date = models.DateField()
+    
+
+    @property
+    def isMine(self):
+        return self.__isMine
+
+    @isMine.setter
+    def isMine(self, value):
+        self.__isMine = value
--- rare_api/views/comment.py
+++ rare_api/views/comment.py
@@ -6,6 +6,8 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.decorators import action
 from rare_api.models import Comment, RareUser, Post
 
 class CommentView(ViewSet):
@@ -17,15 +19,58 @@ class CommentView(ViewSet):
         Returns:
             Response -- JSON serialized list of comments
         """
-        comments = Comment.objects.all()
+        #comments = Comment.objects.all()
+        #post_comment = self.request.query_params.get('post', None)
+        #if post_comment is not None:
+            #comments = comments.filter(post__id=post_comment)
+
+        #serializer = CommentSerializer(
+            #comments, many=True, context={'request': request})
+        #return Response(serializer.data)
+        
+        rare_user = RareUser.objects.get(user=request.auth.user)
+
+        #filtering comments by user
+        rare_user_param = self.request.query_params.get('rare_user', None)
+        if rare_user_param is not None:
+            comments = Comment.objects.filter(author__id=rare_user_param)
+        else:
+            comments = Comment.objects.all()
+
+        for comment in comments:
+            if rare_user == comment.author:
+                comment.isMine = True
+            else:
+                comment.isMine = False
 
         serializer = CommentSerializer(
-            comments, many=True, context={'request': request})
+            comments, many=True, context={'request': request}
+        )
+        return Response(serializer.data)
+
+    @action(methods=['get'], detail=False)
+    def my_comments(self, request, pk=None):
+
+        rare_user = RareUser.objects.get(user=request.auth.user)
+        comments = Comment.objects.filter(author__id=rare_user.id)
+
+        for comment in comments:
+                comment.isMine = True
+
+        serializer = CommentSerializer(
+            comments, many=True, context={'request': request}
+        )
         return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         try:
             comment = Comment.objects.get(pk=pk)
+            author = RareUser.objects.get(user=request.auth.user)
+            if author == comment.author:
+                comment.isMine = True
+            else:
+                comment.isMine = False
+
             serializer = CommentSerializer(comment, context={'request': request})
             return Response(serializer.data)
         except Exception as ex:
@@ -39,6 +84,7 @@ class CommentView(ViewSet):
         post = Post.objects.get(pk=request.data["post"])   
         comment = Comment()
         comment.content = request.data["content"]
+        comment.publication_date = request.data["publication_date"]
         comment.author = user
         comment.post = post
         
@@ -83,6 +129,7 @@ class CommentView(ViewSet):
         user = RareUser.objects.get(user=request.auth.user)
         post = Post.objects.get(pk=request.data["post"])  
         comment.content = request.data["content"]
+        comment.publication_date = request.data["publication_date"]
         comment.author = user
         comment.post = post
 
@@ -90,11 +137,21 @@ class CommentView(ViewSet):
 
         return Response({}, status=status.HTTP_204_NO_CONTENT)
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username']
+
+class RareUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
+    class Meta:
+        model = RareUser
+        fields = ['id', 'user']
+
 
 class CommentSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Comment
-        fields = '__all__'
+        fields = ('id', 'post', 'author', 'content', 'isMine')
         depth = 1
         
