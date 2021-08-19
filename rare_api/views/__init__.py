e68ad4cf002076b55091cf74799a0869acd0f0b7
Val Currie <contact@va.lcuri.email>
2021-08-19 15:00:06 -0500

    implemented list for rare users at "/authors" endpoint

--- rare_api/views/__init__.py
+++ rare_api/views/__init__.py
@@ -6,4 +6,4 @@ from .post import PostView
 from .post_reaction import PostReactionView
 from .auth import login_user, register_user
 from .post_tag import PostTagView
-
+from .rare_user import RareUserView
